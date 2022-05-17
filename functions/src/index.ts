4da60d156ff61910168404d8c94af41ef0a3bc7f
Kawin Glomjai <kawin.glomjai@scbabacus.com>
2022-05-17 10:56:16 +0700

    WIP

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,7 @@
 import * as functions from 'firebase-functions'
+import * as admin from 'firebase-admin'
+
+admin.initializeApp()
 
 const fun = functions.region('asia-southeast1')
 
@@ -10,4 +13,13 @@ const fun = functions.region('asia-southeast1')
 //   response.send("Hello from Firebase!");
 // });
 
-export const saveUserInfo = fun.https.onRequest
+export const saveUserInfo = fun.auth.user().onCreate(async (user) => {
+    try {
+        const writeResult = await admin.firestore().collection('users').doc(user.uid).set({
+            createdAt: new Date(),
+        })
+        console.log(writeResult)
+    } catch (error) {
+        console.error(error)
+    }
+})
