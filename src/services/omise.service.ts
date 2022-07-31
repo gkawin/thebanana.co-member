762ad0568845f7346988651d9253881037c798a3
Kawin Glomjai <kawin.glomjai@scbabacus.com>
2022-07-31 20:18:13 +0700

    use axios instead

--- src/services/omise.service.ts
+++ src/services/omise.service.ts
@@ -1,22 +1,46 @@
-import omise from 'omise'
+import axios, { AxiosInstance } from 'axios'
+import type { Charges } from 'omise'
 import { singleton } from 'tsyringe'
 
 @singleton()
 export class OmiseService {
-    #instance: omise.IOmise
+    #api: AxiosInstance = null
+
+    protected omiseVersion: '2019-05-29'
+
+    private static instance: OmiseService = null
+
+    private static init() {
+        if (OmiseService.instance === null) {
+            OmiseService.instance = new OmiseService()
+        }
+        return OmiseService.instance
+    }
 
     constructor() {
-        this.#instance = omise({
-            publicKey: process.env.OMISE_PUBLIC_KEY,
-            secretKey: process.env.OMISE_SECRET_KEY,
+        this.#api = axios.create({
+            baseURL: 'https://api.omise.co',
+            headers: {
+                'User-Agent': `omise-nodejs/${process.env.npm_package_version ?? '0.0.1'}`,
+                'Omise-Version': this.omiseVersion,
+            },
+            auth: {
+                username: process.env.OMISE_SECRET_KEY,
+                password: '',
+            },
         })
     }
 
     get charges() {
-        return this.#instance.charges
+        const instance = OmiseService.init()
+        return {
+            create: (req: Charges.IRequest) =>
+                instance.#api.post('/charges', req, { headers: { Authorization: 'Basic' } }),
+        }
     }
 
-    get customers() {
-        return this.#instance.customers
-    }
+    // get customers() {
+    //     const instance = OmiseService.init()
+    //     return instance.customers
+    // }
 }
